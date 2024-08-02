f3076b819c4510822f4b85031020d3527d6431c1
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-08-02 22:06:37 +0200

    Update ErrorHandler.js

--- src/middlewares/ErrorHandler.js
+++ src/middlewares/ErrorHandler.js
@@ -1,4 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 export const errorMiddleware = (err, req, res, next) => {
-  res.status(500).send(err.message);
+  res.status(500).send({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
 };
